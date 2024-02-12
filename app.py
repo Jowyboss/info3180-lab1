52ebd6866b1131f82a73974d9d06b01bb87ce6d5
Jowayne Anderson <jowayneaanderson@gmail.com>
2024-02-11 22:32:15 -0500

    created my first template file

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
 from flask import Flask, render_template
-
-app = Flask(__name__)
-
+app = Flask(__name__) 
 '''
 # Routing for your application.
 # Put your routes below this comment
 '''
-
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
-
 
 @app.route('/')
 def home():
@@ -22,4 +12,11 @@ def home():
 
 @app.route('/about')
 def about():
- return render_template('about.html') 
+ return render_template('about.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
